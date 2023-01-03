4ea6ee73d49374e04bd770ae955ccf8984ec2e08
Venirails <krishnaveni@RFL267.local>
2023-01-03 12:39:52 +0530

    alignment changes

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -16,7 +16,7 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
   const submitHandler = (event) => {
-    event.preventDefault();
+        event.preventDefault();
 
     const expenseData = {
         title: enteredTitle,
@@ -34,7 +34,7 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -42,6 +42,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -50,12 +51,14 @@ const ExpenseForm = () => {
           <input
             type="date"
             min="2022-01-01"
-            step="2023-12-31" onChange={dateChangeHandler}
+            step="2023-12-31" 
+            value={enteredDate}
+            onChange={dateChangeHandler}
           />
-        </div>
-        <div className="new-expense__actions">
+        </div>       
+      </div>
+      <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
-        </div>
       </div>
     </form>
   );
