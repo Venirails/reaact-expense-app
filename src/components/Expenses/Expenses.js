c57d5d08abe02c94116bc29948874954ffa5f950
Venirails <krishnaveni@RFL267.local>
2023-01-10 17:54:12 +0530

    assignment 3 completed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,12 +5,20 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
+  
+  const filteredExpenses = props.items.filter(expense => {
+     if(!(filteredYear === ''))
+     {return expense.date.getFullYear().toString() === filteredYear;}
+     else
+     {return expense}
+  });
+
   return (
     <div>
       <Card className="expenses">
@@ -18,8 +26,9 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             date={expense.date}
             amount={expense.amount}
