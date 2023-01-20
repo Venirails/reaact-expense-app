06520f44d232597cf79f82be45c00f62441b11ea
Venirails <krishnaveni@RFL267.local>
2023-01-20 23:03:28 +0530

    Chart component added

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) => {
+  const [isToggle,setIsToggle] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
       const expenseData =  {
       ...enteredExpenseData,
        id: Math.random().toString()
       };
       props.onAddExpense(expenseData);
     };
+  const startToggleHandler = () =>{
+    setIsToggle(true);
+  };
+  const stopToggleHandler = () =>{
+    setIsToggle(false);
+  };
 return <div className='new-expense'>
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+    {!isToggle && <button onClick={startToggleHandler}>Add New Expense</button>}
+    {isToggle && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopToggleHandler}/>}
 </div>
 };
 
